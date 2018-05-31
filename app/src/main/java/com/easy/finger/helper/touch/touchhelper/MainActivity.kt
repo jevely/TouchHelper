fab038723d0a1259857ec5e3ae22b00c69676235
jevely <lijiawei_855@126.com>
2018-05-31 23:27:01 +0800

    项目开启

---
+++ app/src/main/java/com/easy/finger/helper/touch/touchhelper/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.easy.finger.helper.touch.touchhelper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
